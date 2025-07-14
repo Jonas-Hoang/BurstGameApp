428403cc11eef99003a839f82335fa0e91c78fa2
Jonas-Hoang <bangbang3951@gmail.com>
2025-07-14 23:23:42 +0700

    Initial Commit

---
+++ BurstGame/BurstGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurstGameApp.swift
+//  BurstGame
+//
+//  Created by Jonas Hoang on 14/7/25.
+//
+
+import SwiftUI
+
+@main
+struct BurstGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
